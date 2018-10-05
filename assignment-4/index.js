eb74310193e144aecbf0e029ca6c922da3f4301f
rijkvanzanten <rijkvanzanten@me.com>
2018-10-04 22:05:39 -0400

    Finish assignment

--- assignment-4/index.js
+++ assignment-4/index.js
@@ -26,37 +26,105 @@ zones.forEach(({ code, neighborhoods }) => {
   });
 });
 
+const meetingTypes = {
+  b: "Beginners Meeting",
+  bb: "Big Book Meeting",
+  c: "Closed Discussion",
+  o: "Open Meeting",
+  od: "Open Discussion Meeting",
+  s: "Step Meeting"
+};
+
 async function insert() {
-  await Promise.all(zoneCodes.map(code => pool.query(`INSERT INTO zones (id) VALUES ('${code}')`)));
-  
-  await Promise.all(parsedNeighborhoods.map(async ({ name, zone_id, zipcodes }) => {
-    const { rows } = await pool.query(`INSERT INTO neighborhoods (name, zone_id) VALUES ($1::text, '${zone_id}') RETURNING id`, [name]);
-    const neighborhood_id = rows[0].id;
+  await Promise.all(
+    zoneCodes.map(code =>
+      pool.query(`INSERT INTO zones (id) VALUES ('${code}')`)
+    )
+  );
 
-    return await Promise.all(zipcodes.map(zipcode => pool.query(`INSERT INTO zipcodes (zipcode, neighborhood_id) VALUES ('${zipcode}', ${neighborhood_id})`)));
-  }));
+  await Promise.all(
+    Object.keys(meetingTypes).map(type => 
+      pool.query(`
+        INSERT INTO meeting_types (id, name)
+        VALUES ($1, $2)
+        ON CONFLICT ON CONSTRAINT meeting_types_pkey 
+        DO NOTHING;
+      `,[type, meetingTypes[type]]
+      )
+    )
+  );
 
-  await Promise.all(records.map(async ({ building, address, wheelchair, title, details, hours }) => {
-    const { line1, line2, zip, city, state, coordinates } = address;
+  await Promise.all(
+    parsedNeighborhoods.map(async ({ name, zone_id, zipcodes }) => {
+      const { rows } = await pool.query(
+        `INSERT INTO neighborhoods (name, zone_id) VALUES ($1::text, '${zone_id}') RETURNING id`,
+        [name]
+      );
+      const neighborhood_id = rows[0].id;
 
-    const { rows: addressRows } = await pool.query(`
-      INSERT INTO locations (name, line1, line2, zip, city, state, latitude, longitude, wheelchair_accessible, zone_id)
-      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
-      ON CONFLICT ON CONSTRAINT locations_line1_key 
-      DO UPDATE SET line1 = $2
-      RETURNING id;
-    `, [building, line1, line2, zip, city, state, coordinates.lat, coordinates.lon, wheelchair, zone]);
+      return await Promise.all(
+        zipcodes.map(zipcode =>
+          pool.query(
+            `INSERT INTO zipcodes (zipcode, neighborhood_id) VALUES ('${zipcode}', ${neighborhood_id})`
+          )
+        )
+      );
+    })
+  );
 
-    const addressID = addressRows[0].id;
+  await Promise.all(
+    records.map(
+      async ({ building, address, wheelchair, title, details, hours }) => {
+        const { line1, line2, zip, city, state, coordinates } = address;
 
-    const { rows: meetingRows } = await pool.query(`
-      INSERT INTO meetings (title, details, location_id)
-      VALUES ($1, $2, $3)
-      RETURNING id;
-    `, [title, details, addressID]);
+        const { rows: addressRows } = await pool.query(`
+          INSERT INTO locations (name, line1, line2, zip, city, state, latitude, longitude, wheelchair_accessible, zone_id)
+          VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+          ON CONFLICT ON CONSTRAINT locations_line1_key 
+          DO UPDATE SET line1 = $2
+          RETURNING id;
+        `, [
+            building,
+            line1,
+            line2,
+            zip,
+            city,
+            state,
+            coordinates.lat,
+            coordinates.lon,
+            wheelchair,
+            zone
+          ]
+        );
 
-    const meetingID = meetingRows[0].id;
-  }));
+        const addressID = addressRows[0].id;
+
+        const { rows: meetingRows } = await pool.query(`
+          INSERT INTO meetings (title, details, location_id)
+          VALUES ($1, $2, $3)
+          RETURNING id;
+        `, [title, details, addressID]
+        );
+
+        const meetingID = meetingRows[0].id;
+
+        await Promise.all(
+          hours.map(
+            async ({
+              day,
+              startTime,
+              endTime,
+              type,
+              specialInterest
+            }) => pool.query(`
+              INSERT INTO meeting_hours (day, start_time, end_time, special_interest, meeting_id, meeting_type_id)
+              VALUES ($1, $2, $3, $4, $5, $6)
+            `, [day, startTime, endTime, specialInterest, meetingID, type])
+          )
+        );
+      }
+    )
+  );
 
   pool.end();
   process.exit(0);
